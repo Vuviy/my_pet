eaf12e8eed98e9316367f77ff2c9cafa1e3a08dd
Vova <vova.banudz@gmail.com>
2023-01-18 17:34:13 +0200

    add views

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <nav>
+        <ul>
+            <li>footer link 1</li>
+            <li>footer link 2</li>
+            <li>footer link 3</li>
+        </ul>
+    </nav>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <nav>
+        <ul>
+            <li>header link 1</li>
+            <li>header link 2</li>
+            <li>header link 3</li>
+        </ul>
+    </nav>
+</div>
